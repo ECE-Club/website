d1a4018c8b03275a5e4a92fa7c2e441fe2571f17
xlgraceli <xlgraceli@gmail.com>
2025-03-04 18:58:13 -0500

    Updated event page: ping pong tournament

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -100,7 +100,82 @@ function Events() {
                 Upcoming Events:
             </Typography>
             <br/>
-            
+            <BrowserView>
+                <div style={{ borderRadius: "20px", background: "white", padding: "20px", margin: "10px 30px 18px 30px" }}>
+                    <Typography>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                            <div onClick={() => goToUrl("https://docs.google.com/forms/d/1YA1mwKiwX8piGdrC7PJ7GPpY6cPCdDaGLDLckC2f_cA/viewform?edit_requested=true")}>
+                                <img src="/imgs/event_icons/PingPong.png" style={{ width: "140px", height: "140px" }} />                        
+                            </div>
+                            <div style={{ marginLeft: "20px", width: "calc(100% - 160px)" }}>
+                                <Typography sx={{ cursor:'pointer', color: "#152a51", margin: "10px 0" }} variant="h5">
+                                    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                                        <div onClick={() => goToUrl("https://docs.google.com/forms/d/1YA1mwKiwX8piGdrC7PJ7GPpY6cPCdDaGLDLckC2f_cA/viewform?edit_requested=true")} style={{ fontWeight: 'bold' }}> Ping Pong Tournament </div>
+                                        <div style={{ textAlign: "right" }}> March 7th, 2025 </div>
+                                    </div>
+                                </Typography>
+                                <Typography sx={{ color: "#152a51", margin: "10px 0", overflowWrap: "break-word" }} variant="h6">
+                                    <div style={{textAlign: "justify"}}>
+                                    Midterm season got you stressed? Take a break and unwind with some friendly competition! A Ping Pong Tournament is happening AGAIN on <b>March 7th</b> from <b>6:00-7:30 PM</b> in the ECE Common Room 
+                                    (BA1120). 
+                                    </div>
+                                </Typography>
+                                <Typography
+                                    sx={{ color: "#152a51", margin: "10px 0", overflowWrap: "break-word", textDecoration: 'underline', cursor: 'pointer' }}
+                                    variant="h6"
+                                    onClick={() => goToUrl("https://docs.google.com/forms/d/1YA1mwKiwX8piGdrC7PJ7GPpY6cPCdDaGLDLckC2f_cA/viewform?edit_requested=true")}
+                                >
+                                    Sign up today!
+                                </Typography>
+                                <Typography
+                                    sx={{ color: "#152a51", margin: "10px 0", overflowWrap: "break-word", textDecoration: 'underline', cursor: 'pointer' }}
+                                    variant="h6"
+                                    onClick={() => goToUrl("https://www.instagram.com/p/DGyMXpNPv4m/?hl=en")}
+                                >
+                                    Check out our Instagram for more info!
+                                </Typography>
+                            </div>
+                        </div>
+                    </Typography>
+                </div>
+            </BrowserView>
+            <MobileView>
+                <div style={{ borderRadius: "20px", background: "white", padding: "20px", margin: "10px 30px 18px 30px" }}>
+                    <Typography>
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <div onClick={() => goToUrl("https://docs.google.com/forms/d/1YA1mwKiwX8piGdrC7PJ7GPpY6cPCdDaGLDLckC2f_cA/viewform?edit_requested=true")}>
+                                <img src="/imgs/event_icons/PingPong.png" style={{ width: "200px", height: "200px" }} />                        
+                            </div>
+                            <div style={{ marginTop: "10px", width: "100%" }}>
+                                <Typography sx={{ cursor:'pointer', color: "#152a51", margin: "5px 0" }} variant="h5">
+                                    <div onClick={() => goToUrl("https://docs.google.com/forms/d/1YA1mwKiwX8piGdrC7PJ7GPpY6cPCdDaGLDLckC2f_cA/viewform?edit_requested=true")} style={{ fontWeight: 'bold' }}> Ping Pong Tournament </div>
+                                    <div style={{ textAlign: "right" }}> March 7th, 2025 </div>
+                                </Typography>
+                                <Typography sx={{ color: "#152a51", margin: "5px 8px", overflowWrap: "break-word" }} variant="h7">
+                                    <div style={{textAlign: "justify"}}>
+                                        Midterm season got you stressed? Take a break and unwind with some friendly competition! A Ping Pong Tournament is happening AGAIN on <b>March 7th</b> from <b>6:00-7:30 PM</b> in the ECE Common Room 
+                                        (BA1120). 
+                                    </div>
+                                </Typography>
+                                <Typography
+                                    sx={{ color: "#152a51", margin: "10px 0", overflowWrap: "break-word", textDecoration: 'underline', cursor: 'pointer' }}
+                                    variant="h6"
+                                    onClick={() => goToUrl("https://docs.google.com/forms/d/1YA1mwKiwX8piGdrC7PJ7GPpY6cPCdDaGLDLckC2f_cA/viewform?edit_requested=true")}
+                                >
+                                    Sign up today!
+                                </Typography>
+                                <Typography
+                                    sx={{ color: "#152a51", margin: "10px 0", overflowWrap: "break-word", textDecoration: 'underline', cursor: 'pointer' }}
+                                    variant="h6"
+                                    onClick={() => goToUrl("https://www.instagram.com/p/DGyMXpNPv4m/?hl=en")}
+                                >
+                                    Check out our Instagram for more info!
+                                </Typography>
+                            </div>
+                        </div>
+                    </Typography>
+                </div>
+            </MobileView>
             
             <Typography sx={{ fontWeight: 'bold', color: "white", margin: "30px 30px -20px 30px" }} variant="h6">
                 Past Events:
